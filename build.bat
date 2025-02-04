1d8057bcba38d9643fbda189ec08ddee006eae5b
Ryan "RyDawgE" Ebert <59522243+RyDawgE@users.noreply.github.com>
2025-02-04 18:34:19 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,9 +1,12 @@
-@echo OFF
+@echo ON
 
-call "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvars64.bat"
+echo includes :
+echo %INCLUDE%
 
-set includes=/Isrc /I".\include"
-set links=/link /LIBPATH:".\lib" glfw3.lib user32.lib vcruntime.lib msvcrt.lib shell32.lib gdi32.lib
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+set includes=/Isrc /I"include"
+set links=/link /LIBPATH:"lib" glfw3.lib user32.lib vcruntime.lib msvcrt.lib shell32.lib gdi32.lib
 
 cl.exe /Fo:"./" /Fe:"./build/poneros.exe" %includes% %defines% ./src/main.cpp %links% /NODEFAULTLIB:LIBCMT
 
