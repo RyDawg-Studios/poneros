7082678a4db464b8543a54579c3795de10de7a6d
Ryan "RyDawgE" Ebert <RyDawgE515@gmail.com>
2025-02-11 13:10:51 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,9 +1,3 @@
 @echo OFF
 
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-
-set includes=/Isrc /I"include"
-set links=/link /LIBPATH:"lib" glfw3.lib user32.lib vcruntime.lib msvcrt.lib shell32.lib gdi32.lib
-
-cl.exe /Fo:"./" /Fe:"./build/poneros.exe" %includes% %defines% ./src/main.cpp %links% /NODEFAULTLIB:LIBCMT
-
+"C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/Tools/VsDevCmd.bat" && msbuild .\poneros.sln
